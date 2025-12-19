214440d0969b6909cbe004bf6cd6f36a01aa8428
Giuseppe Certo <giuseppe.certo@sdataway.com>
2025-12-19 14:33:49 +0100

    Update manifest and version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "co'clock",  # The name that will appear in the App list
-    "version": "1.2.0",  # Version
+    "version": "1.3.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base", "account"],  # dependencies
     "data": [
